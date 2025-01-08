c922f7f999ce4515417a8f396e864dc4df494ae7
farnoodlotfali <farnoodlotfali@gmail.com>
2025-01-08 17:23:09 +0330

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router";
-import { lazy, Suspense } from "react";
-import Spinner from "./components/Spinner";
-
-const LazyElement = lazy(() => import(`./pages/limitMap`));
-const LazyElement1 = lazy(() => import(`./pages/freeMap`));
+import { RouterProvider } from "react-router";
+import { router } from "./router";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <LazyElement />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/free"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <LazyElement1 />
-            </Suspense>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/router.tsx
@@ -0,0 +1,38 @@
+import { lazy, Suspense } from "react";
+import { createBrowserRouter } from "react-router";
+import ErrorElement from "./components/ErrorElement";
+import Spinner from "./components/Spinner";
+
+const lazyLoadRoutes = (componentName: string, src = "pages") => {
+  const LazyElement = lazy(() => import(`./${src}/${componentName}.tsx`));
+
+  return (
+    <Suspense
+      fallback={
+        <div className="h-dvh bg-white">
+          <Spinner />
+          <div className="text-center">در حال بارگذاری..</div>
+        </div>
+      }
+    >
+      <LazyElement />
+    </Suspense>
+  );
+};
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorElement />,
+    children: [
+      {
+        index: true,
+        element: lazyLoadRoutes("limitMap"),
+      },
+      {
+        element: lazyLoadRoutes("freeMap"),
+        path: "free",
+      },
+    ],
+  },
+]);
